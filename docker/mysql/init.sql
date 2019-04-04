72f2b1ac4828f30d652c8c4e45527a70ca26b186
zloyleva <levchenko.topdevs@gmail.com>
2019-04-04 14:46:38 +0300

    add yourperfectfood.com

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE IF NOT EXISTS `books-yoursmartchef.com`;
-CREATE DATABASE IF NOT EXISTS `books-yourpersonalchiefye;yf gjvjom.com`;
+CREATE DATABASE IF NOT EXISTS `books-yourpersonalchief.com`;
+CREATE DATABASE IF NOT EXISTS `books-yourperfectfood.com`;

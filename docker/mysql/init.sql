786ca6ae98132f88697b4aa5206b7410d0974b65
zloyleva <levchenko.topdevs@gmail.com>
2019-04-09 13:43:52 +0300

    findyourfoodstyle.com

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -2,3 +2,4 @@ CREATE DATABASE IF NOT EXISTS `books-yoursmartchef.com`;
 CREATE DATABASE IF NOT EXISTS `books-yourpersonalchief.com`;
 CREATE DATABASE IF NOT EXISTS `books-yourperfectfood.com`;
 CREATE DATABASE IF NOT EXISTS `books-inspireforcooking.com`;
+CREATE DATABASE IF NOT EXISTS `books-findyourfoodstyle.com`;
